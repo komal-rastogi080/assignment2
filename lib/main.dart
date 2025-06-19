b77cd93686d34812e4c86cb89c113e176d101082
Komal <rastogikomal57@gmail.com>
2025-06-20 01:17:19 +0530

    created project

--- lib/main.dart
+++ lib/main.dart
@@ -11,112 +11,203 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      debugShowCheckedModeBanner: false,
+      title: 'Profile Card',
+      home: Scaffold(
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsetsGeometry.all(14.0),
+            child: Column(
+              children: [
+                ListTile(
+                  title: Center(
+                    child: Text('Profile', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20))
+                  ),
+                  trailing: Icon(Icons.settings),
+                ),
+                SizedBox(height: 20),
+                Center(
+                  child: Column(
+                    children: [
+                      CircleAvatar(
+                        radius: 70,
+                        backgroundImage: AssetImage('assets/images/avatar1.jpg'),
+                      ),
+                      SizedBox(height: 18),
+                      Text('Komal Rastogi', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+                      Text('Product Designer', style: TextStyle(fontSize: 16, color: Colors.indigoAccent),),
+                      Text('San Francisco, CA', style: TextStyle(fontSize: 16, color: Colors.indigoAccent),)
+                    ],
+                  ),
+                ),
+                SizedBox(height: 20),
+                Align(
+                  alignment: Alignment.topLeft,
+                    child: Text('Skills', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold))
+                ),
+                SizedBox(height: 16),
+                Wrap(
+                  runSpacing: 15,
+                  direction: Axis.horizontal ,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 16.0),
+                      height: 40, width: 130,
+                      decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: Colors.grey.shade200),
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Text('UI/UX Design', style: TextStyle(fontSize: 16, color: Colors.black54))
+                      )
+                    ),
+                    SizedBox(width: 14),
+                    Container(
+                        padding: EdgeInsets.symmetric(horizontal: 16.0),
+                        height: 40, width: 170,
+                        decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: Colors.grey.shade200),
+                        child: Align(
+                            alignment: Alignment.center,
+                            child: Text('User Research', style: TextStyle(fontSize: 16, color: Colors.black54))
+                        )
+                    ),
+                    SizedBox(width: 14),
+                    Container(
+                        padding: EdgeInsets.symmetric(horizontal: 16.0),
+                        height: 40, width: 120,
+                        decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: Colors.grey.shade200),
+                        child: Align(
+                            alignment: Alignment.center,
+                            child: Text('Prototyping', style: TextStyle(fontSize: 16, color: Colors.black54))
+                        )
+                    ),
+                    SizedBox(width: 14),
+                    Container(
+                        padding: EdgeInsets.symmetric(horizontal: 16.0),
+                        height: 40, width: 150,
+                        decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: Colors.grey.shade200),
+                        child: Align(
+                            alignment: Alignment.center,
+                            child: Text('Wireframing', style: TextStyle(fontSize: 16, color: Colors.black54))
+                        )
+                    ),
+                    SizedBox(width: 14),
+                    Container(
+                        padding: EdgeInsets.symmetric(horizontal: 16.0),
+                        height: 40, width: 150,
+                        decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: Colors.grey.shade200),
+                        child: Align(
+                            alignment: Alignment.center,
+                            child: Text('Design Systems', style: TextStyle(fontSize: 16, color: Colors.black54))
+                        )
+                    ),
+                    SizedBox(width: 14),
+                    Container(
+                        padding: EdgeInsets.symmetric(horizontal: 16.0),
+                        height: 40, width: 170,
+                        decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: Colors.grey.shade200),
+                        child: Align(
+                            alignment: Alignment.center,
+                            child: Text('Interaction Design', style: TextStyle(fontSize: 16, color: Colors.black54))
+                        )
+                    ),
+                  ],
+                ),
+                SizedBox(height: 40),
+                Align(
+                    alignment: Alignment.topLeft,
+                    child: Text('About', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold))
+                ),
+                SizedBox(height: 30),
+                Text('Komal is a product designer with over 5 years of'
+                    'experience in creating user-centered designs. '
+                    'She specializes in UI/UX design, user research'
+                    ' and prototyping. Komal is passionate about '
+                    'solving complex problems and creating intuitive and engaging user experiences.', style: TextStyle(fontSize: 16)),
+                SizedBox(height: 40),
+                Align(
+                    alignment: Alignment.topLeft,
+                    child: Text('Contact', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold))
+                ),
+                SizedBox(height: 30),
+                Row(
+                     children: [
+                       Container(
+                         width: 50, height: 50,
+                          decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: Colors.grey.shade200),
+                          child:
+                                         Align(
+                        alignment: Alignment.center,
+                        child:Icon(Icons.mail_outline_sharp))
+                       ),
+                  SizedBox(width: 12),
+                  Text('komal.rastogi_cs23@gmail.com'),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Row(
+                    children: [
+                      Container(
+                          width: 50, height: 50,
+                        decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: Colors.grey.shade200),
+                          child:
+                      Align(
+                          alignment: Alignment.center,
+                          child:Icon(Icons.phone))
+                      ),
+                      SizedBox(width: 12),
+                      Text('(+91) 88736 47422'),
+                    ],
+                ),
+                SizedBox(height: 20),
+                Row(
+                     children: [
+                       Container(
+                           width: 50, height: 50,
+                          decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: Colors.grey.shade200),
+                          child:
+                                         Align(
+                          alignment: Alignment.center,
+                          child:Icon(Icons.insert_link))
+                       ),
+                       SizedBox(width: 12),
+                       Text('likedin.com/in/komalrastogi'),
+                     ],
+                   ),
+              ],
+            ),
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.add),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.bookmarks_outlined),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person_outline_outlined),
+              label: '',
             ),
           ],
+          type: BottomNavigationBarType.fixed, // Ensures all icons are shown
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        // bottomNavigationBar: BottomNavigationBar(
+        //   type: BottomNavigationBarType.fixed,
+        //   items: [
+        //     BottomNavigationBarItem(icon: Icon(Icons.abc), label: '')
+        //   ],
+        // ),
+      )
     );
   }
 }
